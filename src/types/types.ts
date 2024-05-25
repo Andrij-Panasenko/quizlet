4ec55c4fed3149e52ffbd4a4299824aff9d5524c
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-25 20:48:42 +0300

    upd prettier, create types

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type Answer = {
+    text: string;
+    correct: boolean;
+}
+
+export type Question = {
+    question: string;
+    answers: Answer[];
+}
+
+export type Quiz = {
+    quizTitle: string;
+    questions: Question[];
+}
+
