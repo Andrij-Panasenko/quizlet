cd9ffa6878385b50f4ea4f199000ed20bd2d2183
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-26 20:53:49 +0300

    add saving quizzes list to state

--- src/components/Quizlets/QuizletList/QuizletList.tsx
+++ src/components/Quizlets/QuizletList/QuizletList.tsx
@@ -1,23 +1,31 @@
+import { useEffect, useState } from 'react';
 import { QUIZZES_KEY } from '../../../helpers/storageKey';
 import { QuizletItem } from '../QuizletItem/QuizletItem';
 import { Quiz } from 'types/types';
 
 export const QuizletList = () => {
-  const getStoragedQuiz = window.localStorage.getItem(QUIZZES_KEY);
+  const [quizes, setQuizes] = useState<Quiz[]>([]);
 
-  const parsedQuizzes = getStoragedQuiz ? JSON.parse(getStoragedQuiz) : [];
+  useEffect(() => {
+    const getStoragedQuiz = window.localStorage.getItem(QUIZZES_KEY);
+
+    const parsedQuizzes = getStoragedQuiz ? JSON.parse(getStoragedQuiz) : [];
+
+    setQuizes(parsedQuizzes);
+  }, []);
+
 
   return (
     <>
       <div className="container px-4 py-8 m-auto">
-        {parsedQuizzes.length === 0 ? (
+        {quizes.length === 0 ? (
           <h2 className="font-bold uppercase text-2xl max-w-96 text-center flex mx-auto">
             Seems you don't have any quizzes so far. Please add some in main
             page
           </h2>
         ) : (
           <ul className="flex flex-wrap gap-3 my-8">
-            {parsedQuizzes.map((quiz: Quiz) => (
+            {quizes.map((quiz: Quiz) => (
               <QuizletItem key={quiz.quizTitle} data={quiz} />
             ))}
           </ul>
