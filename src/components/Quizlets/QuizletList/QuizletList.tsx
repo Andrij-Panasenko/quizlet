794d53afb37c7f0c4a5628d24f25e3b61471f35b
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-27 00:28:15 +0300

    filtration quizzes

--- src/components/Quizlets/QuizletList/QuizletList.tsx
+++ src/components/Quizlets/QuizletList/QuizletList.tsx
@@ -5,6 +5,7 @@ import { Quiz } from 'types/types';
 
 export const QuizletList = () => {
   const [quizes, setQuizes] = useState<Quiz[]>([]);
+  const [filterValue, setFilterValue] = useState<string>('');
 
   useEffect(() => {
     const getStoragedQuiz = window.localStorage.getItem(QUIZZES_KEY);
@@ -20,9 +21,23 @@ export const QuizletList = () => {
     window.localStorage.setItem(QUIZZES_KEY, JSON.stringify(newQuizes));
   };
 
+  const filteredQuizzes = quizes.filter((quiz) => {
+    const hasQuiz = quiz.quizTitle
+      .toLocaleLowerCase()
+      .includes(filterValue.toLocaleLowerCase());
+    return hasQuiz;
+  });
+
   return (
     <>
       <div className="container px-4 py-8 m-auto">
+        <input
+          type="text"
+          placeholder="Serach quizzes"
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+          className="w-full border border-gray-400 rounded-md mb-4 py-2 px-4 focus:outline-none focus:border-blue-500 bg-gray-100"
+        />
         {quizes.length === 0 ? (
           <h2 className="font-bold uppercase text-2xl max-w-96 text-center flex mx-auto">
             Seems you don't have any quizzes so far. Please add some in main
@@ -30,7 +45,7 @@ export const QuizletList = () => {
           </h2>
         ) : (
           <ul className="flex flex-wrap gap-3 my-8">
-            {quizes.map((quiz: Quiz) => (
+            {filteredQuizzes.map((quiz: Quiz) => (
               <QuizletItem
                 key={quiz.quizTitle}
                 data={quiz}
