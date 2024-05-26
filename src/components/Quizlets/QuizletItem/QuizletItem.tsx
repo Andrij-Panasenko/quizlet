82315e83b27992d18937223d788acd6b34a41958
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-26 20:45:36 +0300

    add prompt to request agreement from user to start quiz

--- src/components/Quizlets/QuizletItem/QuizletItem.tsx
+++ src/components/Quizlets/QuizletItem/QuizletItem.tsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom";
-import { Quiz } from "types/types";
+import { MouseEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { Quiz } from 'types/types';
 
 interface Props {
-  data: Quiz; 
+  data: Quiz;
 }
 
 export const QuizletItem = ({ data }: Props) => {
   const { quizTitle, questions } = data;
+
+  const handleMoveToQuiz = (e: MouseEvent) => {
+    if (
+      !confirm(
+        'By clicking this button quiz will start and timer will be launched. Are you ready?'
+      )
+    ) {
+      e.preventDefault();
+    }
+  };
   return (
     <>
       <li className="px-6 py-6 border-2 border-indigo-400 rounded-lg font-semibold w-[calc((100%-24px)/3)]">
@@ -14,6 +25,7 @@ export const QuizletItem = ({ data }: Props) => {
         <div className="flex justify-between items-center">
           <p>Questions {questions.length}</p>
           <Link
+            onClick={handleMoveToQuiz}
             to={`/quizes/${quizTitle}`}
             className="block px-2 py-1 text-white rounded-lg bg-green-500 hover:bg-green-400 transition"
           >
